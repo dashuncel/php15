5adcffcae931d74c10ccd9eee782666fa8a3f2a9
dashuncel <korovkinada@ya.ru>
2017-08-09 16:02:08 +0800

    v1.0

---
+++ create.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Создание таблицы БД</title>
+    <link rel="stylesheet" href="index.css">
+    <meta charset="utf-8">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+</head>
+<body>
+<ul>
+    <li><a href="create.php">Создание таблицы БД</a></li>
+    <li><a href="list.php">Список таблиц БД</a></li>
+</ul>
+</body>
+</html>
+
+
---
+++ lib.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once 'mydata.php';
+
+$host='localhost';
+$dbport=3306;
+/*
+$user=LOGIN;
+$password=PASSWD;
+$database=LOGIN;
+*/
+$user='root';
+$password='';
+$database='global';
+$errors=[];
+
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false
+];
+
+try
+{
+    $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $user, $password, $opt);
+}
+catch (PDOException $e)
+{
+    $errors[] = 'Ошибка подключения к БД: ' . $e->getMessage() . '<br/>';
+}
+
+function prepareTable($query)
+{
+    global $pdo;
+
+    try {
+        $statement = $pdo->prepare($query);
+        $statement->execute($param);
+    } catch (PDOException $e) {
+        $errors = "Ошибка отправки запроса '$query' к БД: " . $e->getMessage() . '<br/>';
+        return $errors;
+    }
+
+    $rows = $statement->fetchAll();
+
+}
