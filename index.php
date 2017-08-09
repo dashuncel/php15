127c141872e8099a7e980e56ff9cd96fa073a4b8
dashuncel <korovkinada@ya.ru>
2017-08-09 16:44:17 +0800

    v1.0

--- index.php
+++ index.php
@@ -12,22 +12,37 @@ require_once __DIR__.'/lib.php';
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 </head>
 <body>
-<ul>
+<ul class="main">
     <li>
-        <a href="#">Создание таблицы БД</a>
-        <section class="create hide">
-            <form>
-                <input>
-            </form>
+        <a href="#">Создание таблицы в БД</a>
+        <section class="create hidden">
+
         </section>
     </li>
     <li>
         <a href="#">Список таблиц БД</a>
-        <section class="list hide">
+        <section class="list hidden">
 
         </section>
     </li>
 </ul>
+<script>
+    'use strict';
+    $('a').click(function(event) {
+        console.log($(this).next('section').class());
+        $(this).next('section').toggleClass('hidden');
+        if ($(this).hasClass('hidden')) {
+            return;
+        }
+        switch ($(this).attr('class')) {
+            case 'create':
+                break;
+            case 'list':
+                break;
+        }
+    });
+
+</script>
 </body>
 </html>
 
