4b97041c16773622309c65382ed26787a674c4d5
dashuncel <korovkinada@ya.ru>
2017-08-09 16:28:29 +0800

    v1.0

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__.'/lib.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Управление таблицами БД</title>
+    <link rel="stylesheet" href="index.css">
+    <meta charset="utf-8">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+</head>
+<body>
+<ul>
+    <li>
+        <a href="#">Создание таблицы БД</a>
+        <section class="create hide">
+            <form>
+                <input>
+            </form>
+        </section>
+    </li>
+    <li>
+        <a href="#">Список таблиц БД</a>
+        <section class="list hide">
+
+        </section>
+    </li>
+</ul>
+</body>
+</html>
+
